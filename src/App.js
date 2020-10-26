564bd5b3c0a03a442eba9df4338d033b379f5042
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-10-26 19:12:03 -0400

    header work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header /> 
+            </div>
+        );
+    }
 }
 
-export default App;
+// MAIN BRAIN App.js
+    //header.js 
+        //logo image 
+        //div logo text
+        //div logo sub-text
+        //8 image-buttons
+        //hr repeat image
+    //MainImage.js
+        //div title text
+        //div post date text
+        //print image
+        //print text
+        //main image    
+    //Recipe.js
+        //3 paragraph texts (h4)
+    //IngredientList.js
+        //2 column list (12 items)
+        //checked box for each item
+        //background image
+    //BioSection.js
+        //hr repeat image
+        //bio image
+        //author name (h5)
+        //bio text (h5)
+        //share recipe button
+    //Footer.js
+        //div split hr
+        //small logo image
+        //copyright text
+
