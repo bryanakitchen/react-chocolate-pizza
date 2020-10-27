05b8af11502e0664e46b246e94b781ad92f6796d
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-10-26 21:07:46 -0400

    finished adding content and plugging in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './Header.js';
 import MainImage from './MainImage.js';
 import Recipe from './Recipe.js';
 import IngredientsList from './IngredientList.js';
+import BioSection from './BioSections.js';
+import Footer from './Footer.js';
 
 export default class App extends React.Component {
     render() {
@@ -12,38 +14,9 @@ export default class App extends React.Component {
                 <MainImage /> 
                 <Recipe /> 
                 <IngredientsList />
+                <BioSection />
+                <Footer />
             </div>
         );
     }
 }
-
-// MAIN BRAIN App.js
-    //header.js 
-        //logo image 
-        //div logo text
-        //div logo sub-text
-        //8 image-buttons
-        //hr repeat image
-    //MainImage.js
-        //div title text
-        //div post date text
-        //print image
-        //print text
-        //main image    
-    //Recipe.js
-        //3 paragraph texts (h4)
-    //IngredientList.js
-        //2 column list (12 items)
-        //checked box for each item
-        //background image
-    //BioSection.js
-        //hr repeat image
-        //bio image
-        //author name (h5)
-        //bio text (h5)
-        //share recipe button
-    //Footer.js
-        //div split hr
-        //small logo image
-        //copyright text
-
